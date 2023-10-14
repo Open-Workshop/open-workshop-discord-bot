4ff5a0b8b304c7d290c826638267a495b98c3255
Miskler <lysenkomiha913@gmail.com>
2023-10-14 20:30:02 +0300

    support OW and delete loader from steam

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from discord.ui import Button
 
 
 SERVER_ADDRESS = 'https://api.openworkshop.su'
+WEBSITE_ADDRESS = 'https://openworkshop.su'
 
 
 class MyClient(discord.Client):
@@ -20,26 +21,6 @@ class MyClient(discord.Client):
         await tree.sync()
         print("Online")
 
-    async def on_guild_join(self, guild):
-        # Получение первого доступного текстового канала
-        channel = next((channel for channel in guild.text_channels if channel.permissions_for(guild.me).send_messages), None)
-
-        if channel:
-            await channel.send("""
-Этот бот позволяет скачивать моды со Steam через чат Discord! 💨
-
-**Разработчики не несут ответственность за контент получаемый через бота и ваши намеренья как его использовать. 📄**
-**А так же оставляя бота на этом сервере вы подтверждаете, что все участники официально приобрели игру/программу на одной из площадок где она представлена! 🛒**
-            """)
-
-            view = discord.ui.View()  # Establish an instance of the discord.ui.View class
-            item = Button(emoji="👀", label="Клик!",
-                          url="https://steamdb.info/sub/17906/apps/")
-            view.add_item(item=item)
-
-            await channel.send("Список поддерживаемых игр", view=view)
-        else:
-            print("Нет доступного канала для отправки сообщения.")
 
 
 activity = discord.Activity(
@@ -93,10 +74,7 @@ async def project(interaction: discord.Interaction):
     item = Button(style=style, emoji="🤩", label="API бота", url=SERVER_ADDRESS)
     view.add_item(item=item)
     
-    item = Button(style=style, emoji="☝", label="Сайт", url="https://openworkshop.su")
-    view.add_item(item=item)
-
-    item = Button(style=style, emoji="👀", label="Список поддерживаемых игр", url="https://steamdb.info/sub/17906/apps/")
+    item = Button(style=style, emoji="☝", label="Сайт", url=WEBSITE_ADDRESS)
     view.add_item(item=item)
 
     await interaction.response.send_message(embed=embedVar, view=view)
@@ -106,16 +84,14 @@ async def project(interaction: discord.Interaction):
 async def download_context(interaction: discord.Interaction, message: discord.Message):
     await main_download(interaction=interaction, link=message.content)
 
-@tree.command(name='download', description="Скачай мод напрямую со Steam передав ссылку на мод или ID мода в Steam!")
+@tree.command(name='download', description="Скачай мод напрямую со Open Workshop передав ссылку на мод или ID мода!")
 async def download(interaction: discord.Interaction, link:str):
     await main_download(interaction=interaction, link=link)
 
 
-# TODO побороть ошибку параллельной загрузку
 # Основной обработчик загрузки модов
 async def main_download(interaction: discord.Interaction, link:str):
     global SERVER_ADDRESS
-    await interaction.response.send_message("В обработке")
 
     channel = client.get_channel(interaction.channel_id)
 
@@ -124,13 +100,13 @@ async def main_download(interaction: discord.Interaction, link:str):
 
         link = await tools.pars_link(link=link)
         if link is bool:
-            await channel.send("Ты мне какую-то не правильную ссылку скинул! 🧐")
+            await interaction.response.send_message("Ты мне какую-то не правильную ссылку скинул! 🧐")
             return
 
         if link.isdigit():
             link = int(link)
             if link <= 0:
-                await channel.send("Я даже без проверки знаю, что такого мода нету :)")
+                await interaction.response.send_message("Я даже без проверки знаю, что такого мода нету :)")
             else:
                 try:
                     async with aiohttp.ClientSession() as session:
@@ -145,135 +121,63 @@ async def main_download(interaction: discord.Interaction, link:str):
 
                             embedVar = discord.Embed(title=f"Ого! `{info['result'].get('name', str(link))}` весит {round(info['result'].get('size', 1)/1048576, 1)} мегабайт!\nСкачай его по прямой ссылке :smirk_cat:")
 
-                            item = Button(style=style, label="Скачать", url=SERVER_ADDRESS+f"/download/{link}")
-                            view.add_item(item=item)
+                            itemD = Button(style=style, label="Скачать", url=SERVER_ADDRESS+f"/download/{link}")
+                            view.add_item(item=itemD)
+                            itemW = Button(style=style, label="Станица на сайте", url=WEBSITE_ADDRESS+f"/mod/{link}")
+                            view.add_item(item=itemW)
 
-                            await channel.send(embed=embedVar, view=view)
+                            await interaction.response.send_message(embed=embedVar, view=view)
                             return
                 except:
-                    await channel.send("Похоже, что сервер не отвечает 😔 _(point=2)_")
+                    await interaction.response.send_message("Похоже, что сервер не отвечает 😔 _(point=2)_")
                     return -1
 
                 try:
                     async with aiohttp.ClientSession() as session:
                         async with session.get(url=SERVER_ADDRESS+f"/download/steam/{str(link)}", timeout=20) as response:
                             if response.headers.get('content-type') == "application/zip":
+                                await interaction.response.send_message("Сейчас пришлю...")
+
                                 file_content = await response.read()
                                 file_name = await tools.get_name(response.headers.get("content-disposition", "ERROR.zip"))
                                 print(f"File name: {file_name}")
+
                                 file = discord.File(io.BytesIO(file_content), filename=file_name)
-                                await channel.send(f"Ваш запрос занял {await tools.format_seconds(round(time.time()-start_time, 1))}", file=file)
+
+                                view = discord.ui.View()  # Establish an instance of the discord.ui.View class
+                                style = discord.ButtonStyle.gray  # The button will be gray in color
+                                itemW = Button(style=style, label="Станица на сайте",
+                                               url=WEBSITE_ADDRESS + f"/mod/{link}")
+                                view.add_item(item=itemW)
+
+                                await channel.send(f"Ваш запрос занял {await tools.format_seconds(round(time.time()-start_time, 1))}", file=file, view=view)
                                 return
                             else:
                                 result = await response.read()
                                 header_result = response.headers
                 except:
                     print("ERROR")
-                    await channel.send("Похоже, что сервер не отвечает 😔 _(point=3)_")
+                    await interaction.response.send_message("Похоже, что сервер не отвечает 😔 _(point=3)_")
                     return -1
 
 
                 if header_result.get('content-type') == "application/json":
                     data = json.loads(result.decode())
-                    if data["error_id"] == 0 or data["error_id"] == 3:
-                        await channel.send("На сервере нету этого мода, но он сейчас его загрузит! _(это может занять некоторое время)_")
-
-                        for i in range(60):
-                            await asyncio.sleep(1)
-                            try:
-                                async with aiohttp.ClientSession() as session:
-                                    response = await session.get(url=SERVER_ADDRESS+f"/condition/mod/%5B{str(link)}%5D",
-                                                                 timeout=10)
-                                    res = await response.read()
-                                    header_result = response.headers
-                            except:
-                                await channel.send("Похоже, что сервер не отвечает 😔 _(point=5)_")
-                                return -1
-                            if header_result.get('content-type') == "application/json":
-                                data = json.loads(res.decode())
-                                if data.get(str(link), None) == None:
-                                    view = discord.ui.View()
-                                    item = Button(emoji="👀", label="Список поддерживаемых игр", url="https://steamdb.info/sub/17906/apps/")
-                                    view.add_item(item=item)
-
-                                    await channel.send("Серверу не удалось загрузить этот мод 😢", view=view)
-                                    return -1
-                                elif data[str(link)] <= 1:
-                                    try:
-                                        async with aiohttp.ClientSession() as session:
-                                            response = await session.get(url=SERVER_ADDRESS + f"/info/mod/{str(link)}",
-                                                                         timeout=10)
-                                            data = await response.text()
-
-                                            # Если больше 30 мб (получаю от сервера в байтах, а значит и сравниваю в них)
-                                            info = json.loads(data)
-                                            if info["result"] is not None and info["result"].get("size", 0) > 10485760:
-                                                view = discord.ui.View()  # Establish an instance of the discord.ui.View class
-                                                style = discord.ButtonStyle.gray  # The button will be gray in color
-
-                                                embedVar = discord.Embed(
-                                                    title=f"Ого! `{info['result'].get('name', str(link))}` весит {round(info['result'].get('size', 1) / 1048576, 1)} мегабайт!\nСкачай его по прямой ссылке :smirk_cat:")
-
-                                                item = Button(style=style, label="Скачать",
-                                                              url=SERVER_ADDRESS + f"/download/{link}")
-                                                view.add_item(item=item)
-
-                                                await channel.send(embed=embedVar, view=view)
-                                                return
-                                    except:
-                                        await channel.send("Похоже, что сервер не отвечает 😔 _(point=4)_")
-                                        return -1
-
-                                    try:
-                                        async with aiohttp.ClientSession() as session:
-                                            async with session.get(url=SERVER_ADDRESS + f"/download/{str(link)}",
-                                                                   timeout=20) as response:
-                                                if response.headers.get('content-type') == "application/zip":
-                                                    file_content = await response.read()
-                                                    file_name = await tools.get_name(
-                                                        response.headers.get("content-disposition", "ERROR.zip"))
-                                                    print(file_name)
-                                                    file = discord.File(io.BytesIO(file_content), filename=file_name)
-                                                    await channel.send(
-                                                        f"Ваш запрос занял {await tools.format_seconds(round(time.time() - start_time, 1))}",
-                                                        file=file)
-                                                    return
-                                                else:
-                                                    view = discord.ui.View()
-                                                    item = Button(emoji="👀", label="Список поддерживаемых игр",
-                                                                  url="https://steamdb.info/sub/17906/apps/")
-                                                    view.add_item(item=item)
-
-                                                    await channel.send("Серверу не удалось загрузить этот мод 😢",
-                                                                       view=view)
-                                    except:
-                                        await channel.send("Похоже, что сервер не отвечает 😔 _(point=1)_")
-
-
-                                    return
-                            else:
-                                await channel.send("Сервер прислал неожиданный ответ 😧 _(point=1)_")
-                                return
-                        await channel.send("Превышено время ожидания ответа с сервера!")
-                        return -1
-
-                    elif data["error_id"] == 1:
-                        await channel.send("Сервер запускается и не может сейчас грузить моды! Повтори попытку через пару минут :)")
-                    elif data["error_id"] == 2:
-                        await channel.send("Сервер говорит что такого мода не существует 😢")
+                    if data["error_id"] in [0, 2, 3]:
+                        await interaction.response.send_message("На сервере нету этого мода :(")
                     else:
-                        await channel.send("Сервер прислал неожиданный ответ 😧 _(point=2)_")
+                        await interaction.response.send_message("Сервер прислал неожиданный ответ 😧 _(point=2)_")
                 else:
-                    await channel.send("Сервер прислал неожиданный ответ 😧 _(point=3)_")
+                    await interaction.response.send_message("Сервер прислал неожиданный ответ 😧 _(point=3)_")
         else:
-            if link is str and (link.startswith("https://steamcommunity.com") or link.startswith("https://store.steampowered.com")):
-                await channel.send("Мне нужна ссылка конкретно на мод! _(или его ID)_")
-            elif link is str and (link.startswith("https://") or link.startswith("http://")):
-                await channel.send("Пока что я умею скачивать только со Steam 😿")
+            if type(link).__name__ == 'str' and (link.startswith("https://steamcommunity.com") or link.startswith("https://store.steampowered.com") or link.startswith("https://openworkshop.su")):
+                await interaction.response.send_message("Мне нужна ссылка конкретно на мод! _(или его ID)_")
+            elif type(link).__name__ == 'str' and (link.startswith("https://") or link.startswith("http://")):
+                await interaction.response.send_message("Пока что я умею скачивать только c Open Workshop и ассоцированные моды со Steam 😿")
             else:
-                await channel.send("Если ты хочешь скачать мод, то просто скинь ссылку или `ID` мода в чат!")
+                await interaction.response.send_message("Если ты хочешь скачать мод, то просто скинь ссылку или `ID` мода в чат!")
     except:
-        await channel.send("Ты вызвал странную ошибку...\nПопробуй загрузить мод еще раз!")
+        await interaction.response.send_message("Ты вызвал странную ошибку...\nПопробуй загрузить мод еще раз!")
 
 
 
